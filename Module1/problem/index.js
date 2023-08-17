67642ec8286d5fc1b3686686b950cad73a418850
malikabusufyan <118253617+malikabusufyan@users.noreply.github.com>
2023-08-17 23:32:10 +0530

    Create HTTP Server

---
+++ Module1/problem/index.js
@@ -0,0 +1,15 @@
+// Please don't change the pre-written code
+// Import the necessary modules here
+
+// Write your code here
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Response received at port 8080.");
+});
+
+server.listen(8080, () => {
+  console.log("Server is Listening to the Port 8000");
+});
+
+module.exports = server;
